1b86b410d9a0746372985f5d48e49fcde03fd9bf
Michael Pavkovic <mpavkovic688@gmail.com>
2016-02-23 19:09:51 -0500

    Fixed issue with reversed red switch.

--- src/org/usfirst/frc/team5422/DSIO/DSIO.java
+++ src/org/usfirst/frc/team5422/DSIO/DSIO.java
@@ -106,7 +106,7 @@ public class DSIO {
         //Put a nullzone on values that are between -0.2 and 0.2
         if (xPos >= -0.2 & xPos <= 0.2) xPos = 0;
 
-        if (ignoreJoystick) xPos = 0;
+        if (!ignoreJoystick) xPos = 0;
         return xPos;
     }
 
@@ -116,7 +116,7 @@ public class DSIO {
         //Put a nullzone on values that are between -0.2 and 0.2
         if (yPos >= -0.2 & yPos <= 0.2) yPos = 0;
 
-        if (ignoreJoystick) yPos = 0;
+        if (!ignoreJoystick) yPos = 0;
         return yPos;
     }
 
@@ -148,7 +148,7 @@ public class DSIO {
         //Put a nullzone on values that are between -0.2 and 0.2
         if (xPosAvg >= -0.2 & xPosAvg <= 0.2) xPosAvg = 0;
 
-        if (ignoreJoystick) xPosAvg = 0;
+        if (!ignoreJoystick) xPosAvg = 0;
         return xPosAvg;
     }
 
@@ -176,7 +176,7 @@ public class DSIO {
         //Put a nullzone on values that are between -0.2 and 0.2
         if (yPosAvg >= -0.2 & yPosAvg <= 0.2) yPosAvg = 0;
 
-        if (ignoreJoystick) yPosAvg = 0;
+        if (!ignoreJoystick) yPosAvg = 0;
         return yPosAvg;
     }
 
@@ -198,7 +198,7 @@ public class DSIO {
         if (xPos >= -0.2 & xPos <= 0.2) xPos = 0;
         if (yPos >= -0.2 & yPos <= 0.2) yPos = 0;
 
-        if (ignoreJoystick) xFinal = 0;
+        if (!ignoreJoystick) xFinal = 0;
         return xFinal;
     }
 
@@ -216,7 +216,7 @@ public class DSIO {
         //Put a nullzone on values that are between -0.2 and 0.2
         if (yPos >= -0.2 & yPos <= 0.2) yPos = 0;
 
-        if (ignoreJoystick) yFinal = 0;
+        if (!ignoreJoystick) yFinal = 0;
         return yFinal;
     }
 
