ce302d66ea3f2cdc0adfe3b5977772b9eab4ed99
Michael Pavkovic <mpavkovic688@gmail.com>
2016-02-26 21:24:03 -0500

    Added button commands for speed/angle sliders.

---
+++ src/org/usfirst/frc/team5422/commands/buttonCommands/AngleSliderCommand.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team5422.commands.buttonCommands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.usfirst.frc.team5422.DSIO.DSIO;
+import org.usfirst.frc.team5422.controller.StrongholdRobot;
+
+/**
+ * @author Michael
+ */
+public class AngleSliderCommand extends Command {
+
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    protected void execute() {
+        if (!DSIO.assistShoot) {
+            System.out.println("Adjusting angle of shooter based on slider");
+
+            double actuatorArmSliderValue = DSIO.getActuatorSliderValue();
+            SmartDashboard.putNumber("Slider Value: ", actuatorArmSliderValue);
+            StrongholdRobot.shooterSubsystem.changeAngle(actuatorArmSliderValue);
+        }
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return true;
+    }
+
+    @Override
+    protected void end() {
+
+    }
+
+    @Override
+    protected void interrupted() {
+
+    }
+}
