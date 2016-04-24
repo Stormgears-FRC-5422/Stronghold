9916d193fc6b0c8195f06f95cb486ed2a8d1dcd9
surenck <surenck@gmail.com>
2016-04-24 11:31:21 -0400

    temporarily commented out Rockwall Navigation Alignment code

--- src/org/usfirst/frc/team5422/defense/RockWall.java
+++ src/org/usfirst/frc/team5422/defense/RockWall.java
@@ -16,19 +16,19 @@ public class RockWall extends DefenseType {
 		
 	}
 
-	protected void navigatorAlignmentToShoot(int posX, int posY, double turnToAngle) {
-		int climbRockWallDistance = 25;
-		int defenseRampDistance = 12;
-		
-		StrongholdRobot.navigatorSubsystem.driveTo(posX, StrongholdConstants.POSITION_DEFENSE_2_REACH[1] + defenseRampDistance);
-		StrongholdRobot.navigatorSubsystem.setRPS(2.0);		
-		StrongholdRobot.navigatorSubsystem.driveTo(posX, climbRockWallDistance);
-		StrongholdRobot.navigatorSubsystem.setRPS(3.0);				
-		StrongholdRobot.navigatorSubsystem.driveTo(posX, posY - (StrongholdConstants.POSITION_DEFENSE_2_REACH[1] + defenseRampDistance + climbRockWallDistance));
-
-		StrongholdRobot.navigatorSubsystem.turnToRelative(Math.toRadians(turnToAngle));
-	}
-
+//	protected void navigatorAlignmentToShoot(int posX, int posY, double turnToAngle) {
+//		int climbRockWallDistance = 25;
+//		int defenseRampDistance = 12;
+//		
+//		StrongholdRobot.navigatorSubsystem.driveTo(posX, StrongholdConstants.POSITION_DEFENSE_2_REACH[1] + defenseRampDistance);
+//		StrongholdRobot.navigatorSubsystem.setRPS(2.0);		
+//		StrongholdRobot.navigatorSubsystem.driveTo(posX, climbRockWallDistance);
+//		StrongholdRobot.navigatorSubsystem.setRPS(3.0);				
+//		StrongholdRobot.navigatorSubsystem.driveTo(posX, posY - (StrongholdConstants.POSITION_DEFENSE_2_REACH[1] + defenseRampDistance + climbRockWallDistance));
+//
+//		StrongholdRobot.navigatorSubsystem.turnToRelative(Math.toRadians(turnToAngle));
+//	}
+//
 	protected int calcExtraDistanceByDefenseType() {
 		return 40;
 	}
